6bff8c29476bb04cd733dd3c05e3553a05948708
rainverted <MY_NAME@example.com>
2021-05-23 14:54:36 +0300

    validations

--- index.js
+++ index.js
@@ -2,13 +2,26 @@ const Validation = require('./js/Validation');
 
 const v = new Validation();
 
+
+//console.log(v.isValidMessage('Some message'), true);
+//console.log(v.isValidMessage(12345), false);
+//console.log(v.isValidMessage('12345'), false);
+
 console.log(v.isValidFirstName('Vardenis'), true);
-console.log(v.isValidLastName('Pavardenis'), true);
-console.log(v.isValidEmail('demo@example.com'), true);
-console.log(v.isValidMessage('Some message'), true);
-console.log(v.isValidPhoneNumber(+37061234567), true);
-console.log(v.isValidMonthName('Sausis'), true);
-console.log(v.isValidMonthName('January'), true);
-console.log(v.isValidWeekdayName('Pirmadienis'), true);
-console.log(v.isValidWeekdayName('Monday'), true);
-console.log(v.isValidPersonID(39912311234), true);
+console.log(v.isValidFirstName(123), false);
+console.log(v.isValidFirstName(''), false);
+console.log(v.isValidFirstName('a'), false);
+console.log(v.isValidFirstName('Raimonda'), true);
+console.log(v.isValidFirstName('raimonda'), false);
+console.log(v.isValidFirstName('RaiMonda'), false);
+console.log(v.isValidFirstName('Raimonda5'), false);
+console.log(v.isValidFirstName('Rai monda'), false);
+
+//console.log(v.isValidLastName('Pavardenis'), true);
+//console.log(v.isValidPhoneNumber('+37061234567'), true);
+//console.log(v.isValidEmail('demo@example.com'), true);
+//console.log(v.isValidMonthName('Sausis'), true);
+//console.log(v.isValidMonthName('January'), true);
+//console.log(v.isValidWeekdayName('Pirmadienis'), true);
+//console.log(v.isValidWeekdayName('Monday'), true);
+//console.log(v.isValidPersonID(39912311234), true);
--- js/Validation.js
+++ js/Validation.js
@@ -1,9 +1,26 @@
 class Validation {
-    isValidFirstName() {
+    isValidFirstName(firstName) {
+        //blogai jeigu ne stringas
+        //blogai jeigu simboliu kiekis yra maziau nei 2
+        //blogai jeigu pirmoji vardo raide mazoji
+               if (!this.isValidMessage(firstName)) {
+            return false;
+        }
+        if (firstName.length < 2) {
+            return false;
+        }
+        if (firstName[0].toLowerCase() === firstName[0]) {
+            return false;
+        }
+        if (firstName.slice(1).toLowerCase() !== firstName.slice(1)) {
+            return false;
+        }
         return true;
     }
 
-    isValidLastName() {
+    isValidLastName(lastName) {
+        //blogai jeigu ne stringas
+        //blogai jeigu simboliu yra maziau nei 2
         return true;
     }
 
@@ -11,11 +28,19 @@ class Validation {
         return true;
     }
 
-    isValidMessage() {
-        return true;
+    isValidMessage(message) {
+        //blogai jeigu ne stringas
+        //blogai jeigu tekstas yra tuscias
+        if (typeof message !== 'string' || message === '') {
+           console.log('ERROR: Message has to be a text string');
+        return false;
+       } 
+       return true;
     }
 
-    isValidPhoneNumber() {
+    isValidPhoneNumber(phoneNumber) {
+        //tipo nustatytmas: string & mumber, & else
+        //
         return true;
     }
 
