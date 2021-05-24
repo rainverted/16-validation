1fbabfdb85dc2c2e5865b712bcdc3f924b0c0f46
rainverted <MY_NAME@example.com>
2021-05-24 20:29:13 +0300

    testing + toBe

---
+++ js/sum/sum.test.js
@@ -0,0 +1,17 @@
+const sum = require('./sum');
+
+test('adds 0 + 0 to equal 0', () => {
+    expect(sum(0, 0)).toBe(0);
+});
+
+test('adds 0.5 + 0.5 to equal 1', () => {
+    expect(sum(0.5, 0.5)).toBeCloseTo(1, 5);
+});
+
+test('adds 0.1 + 0.2 to equal 0.3', () => {
+    expect(sum(0.1, 0.2)).toBeCloseTo(0.3, 5);
+});
+
+test('adds 0.2 + 0.2 to equal 0.4', () => {
+    expect(sum(0.2, 0.2)).toBeCloseTo(0.4, 5);
+});
